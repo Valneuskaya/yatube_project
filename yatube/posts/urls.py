7e77646518315bd6224d36f72e746320a8ba2ee2
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-09 00:38:32 +0300

    Вывод информации из базы данных

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
+    # Главная страница
     path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts')
 ]
