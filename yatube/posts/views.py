af317f3b8b3aa4e968f775b52792ab369901cbb8
Valeri Neuskaya <valerineuskaya@yandex.by>
2021-12-10 21:06:19 +0300

    рабочие group posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,8 +5,10 @@ from .models import Post, Group
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
+    title = "Это главная страница проекта Yatube"
     context = {
        'posts': posts,
+       'title': title,
     }
     return render(request, template, context)
 
@@ -15,8 +17,10 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     template = 'posts/group_list.html'
+    title = "Здесь будет информация о группах проекта Yatube"
     context = {
        'group': group,
        'posts': posts,
+       'title': title,
     }
     return render(request, template, context)
